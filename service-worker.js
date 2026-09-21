a6c1cdde3428b01db024360ce22a1779f4388dd9
fernandocarvalhocms-dotcom <fernando.carvalho.cms@gmail.com>
2026-09-21 09:23:24 -0300

    Atualiza cache para nova agenda de treinos

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='treinos-v2';
+const CACHE='treinos-v3';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
